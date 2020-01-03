99f50c0be8b7b1ac2347925f56ec26b12a57cbff
AnantharamanSI <54242457+AnantharamanSI@users.noreply.github.com>
2020-01-03 16:37:29 +0530

    New version (Race to 2)

--- main.jl
+++ main.jl
@@ -1,5 +1,8 @@
+# Rock-Paper-Scissors Game
+
+
 compWins = 0
-ties = 0
+userWins = 0
 
 function getInput()
   println("Type in rock, paper, or scissors: ")
@@ -16,20 +19,20 @@ function tie(compChoice)
   println("\nYou tied this, the computer chose $compChoice.")
   enter()
   clear()
-  global ties += 1;
 end
 
 function loss(compChoice)
   println("\nYou lost this round, the computer chose $compChoice.")
   enter()
   clear()
-  global compWins += 1;
+  global compWins += 1
 end 
 
 function win(compChoice)
   println("\nYou won this round, the computer chose $compChoice.")
   enter()
   clear()
+  global userWins+=1
 end
 
 function clear()
@@ -40,7 +43,7 @@ end
 println("\nRock-Paper-Scissors Game")
 println("\nJulia")
 println("\n")
-println("\nThis game consists of three rounds of rock-paper-scissors\nplayed against the computer.")
+println("\nThis rock-paper-scissors game is a race to two wins\nplayed against the computer.")
 
 enter()
 println("\n")
@@ -48,8 +51,7 @@ println("\n")
 
 choices = ["rock", "paper", "scissors"]
 
-for i = 1:3
-
+while userWins < 2 && compWins < 2
   userChoice = getInput()
   compChoice = choices[rand(1:3)]
 
@@ -58,10 +60,12 @@ for i = 1:3
   userChoice == "paper" ? compChoice == "paper" ? tie(compChoice) : compChoice == "scissors" ? loss(compChoice) : win(compChoice) : 
   
   userChoice == "scissors" ? compChoice == "scissors" ? tie(compChoice) : compChoice == "rock" ? loss(compChoice) : win(compChoice) : print()
+  println("Current Score")
+  println("You: ",userWins)
+  println("Computer: ",compWins)
+  println()
+
 
 end
 
-ties == 3 ? println("You and the computer tied!") : 
-ties == 2 ? compWins == 1 ? println("You lost.") : println("You won!!") : 
-ties == 1 ? compWins == 0 ? println("You won!!") : compWins == 1 ? println("You and the computer tied!") : println("You lost.") : 
-compWins > 1 ? println("You lost.") : println("You won!!")
+compWins == 2 ? println("You lost.") : println("You win!!")
